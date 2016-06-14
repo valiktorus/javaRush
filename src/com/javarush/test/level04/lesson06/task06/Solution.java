3fc184af65ed78b571fd86ade339c0fb84872f9f
valiktorus <valiktorus@mail.ru>
2016-06-14 13:02:49 +0300

    init

---
+++ src/com/javarush/test/level04/lesson06/task06/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level04.lesson06.task06;
+
+/* И 18-ти достаточно
+Ввести с клавиатуры имя и возраст. Если возраст больше 20 вывести надпись «И 18-ти достаточно».
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+        System.out.println((age>20)? "И 18-ти достаточно":"");
+
+    }
+}
