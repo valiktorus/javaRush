0ccc0aa481679961219f0ef81e71cb84f031be13
valiktorus <valiktorus@mail.ru>
2016-06-14 10:23:49 +0300

    init

---
+++ src/com/javarush/test/level04/lesson04/task04/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level04.lesson04.task04;
+
+/* Время года
+Реализовать метод checkSeason. По номеру месяца, метод должен определить время года (зима, весна, лето, осень) и вывести на экран.
+Пример для номера месяца 2:
+зима
+Пример для номера месяца 5:
+весна
+*/
+
+public class Solution
+{
+    public static void main(String[] args) {
+        checkSeason(12);
+        checkSeason(4);
+        checkSeason(7);
+        checkSeason(10);
+    }
+
+    public static void checkSeason(int mount){
+        //::CODE:
+        System.out.println((mount>=3&&mount<=5)?"весна":(mount>=6&&mount<=8)?"лето":(mount>=9&&mount<=11)?"осень":"зима");
+    }
+}
---
+++ src/com/javarush/test/level04/lesson04/task06/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.test.level04.lesson04.task06;
+
+/* День недели
+Ввести с клавиатуры номер дня недели, в зависимости от номера вывести название «понедельник», «вторник», «среда», «четверг», «пятница», «суббота», «воскресенье»,
+если введен номер больше или меньше 7 – вывести «такого дня недели не существует».
+Пример для номера 5:
+пятница
+Пример для номера 10:
+такого дня недели не существует
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int day = scanner.nextInt();
+        if (day <= 0 || day >=8){
+            System.out.println("такого дня недели не существует");
+        }else {
+            switch (day){
+                case 1:
+                    System.out.println("понедельник");
+                    break;
+                case 2:
+                    System.out.println("вторник");
+                    break;
+                case 3:
+                    System.out.println("среда");
+                    break;
+                case 4:
+                    System.out.println("четверг");
+                    break;
+                case 5:
+                    System.out.println("пятница");
+                    break;
+                case 6:
+                    System.out.println("суббота");
+                    break;
+                case 7:
+                    System.out.println("воскресенье");
+                    break;
+            }
+        }
+
+    }
+
+}
