d9fb76f6e896fa7a0695b01bddfb5772d37de42f
valiktorus <valiktorus@mail.ru>
2016-06-20 12:38:29 +0300

    init

---
+++ src/com/javarush/test/level05/lesson12/home05/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level05.lesson12.home05;
+
+/* Вводить с клавиатуры числа и считать их сумму
+Вводить с клавиатуры числа и считать их сумму, пока пользователь не введёт слово «сумма». Вывести на экран полученную сумму.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+    }
+}
