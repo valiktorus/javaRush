a8ba7c3f604b4ec1065f9ca49e3b5327199d7cbe
Torus <valiktorus@mail.ru>
2016-06-27 21:21:48 +0200

    init

--- src/com/javarush/test/level07/lesson12/home06/Solution.java
+++ src/com/javarush/test/level07/lesson12/home06/Solution.java
@@ -16,6 +16,29 @@ public class Solution
     public static void main(String[] args)
     {
         //напишите тут ваш код
+        Human grandfather1 = new Human("Petia",true,78,null,null);
+        Human grandmother1 = new Human("Masha", false,76,null,null);
+
+        Human grandfather2 = new Human("Vasia", true, 77, null,null);
+        Human grandmother2 = new Human("vasilisa", false, 75, null,null);
+
+        Human father = new Human("Misha", true,48, grandfather1, grandmother1);
+        Human mother = new Human("Katia", false, 46, grandfather2,grandmother2);
+
+        Human son1 = new Human("Anatolii", true,20,father,mother);
+        Human son2 = new Human("Andrei", true,16,father,mother);
+        Human son3 = new Human("Kolia", true,18,father,mother);
+
+        System.out.println(grandfather1);
+        System.out.println(grandmother1);
+        System.out.println(grandfather2);
+        System.out.println(grandmother2);
+
+        System.out.println(father);
+        System.out.println(mother);
+        System.out.println(son1);
+        System.out.println(son2);
+        System.out.println(son3);
     }
 
     public static class Human
@@ -27,10 +50,7 @@ public class Solution
         private Human father = null;
         private Human mother = null;
 
-        public Human(String name, boolean sex, int age){
-            this.name = name;
-            this.sex =sex;
-            this.age = age;
+
 
         public Human(String name, boolean sex, int age, Human father, Human mother){
             this.name = name;
