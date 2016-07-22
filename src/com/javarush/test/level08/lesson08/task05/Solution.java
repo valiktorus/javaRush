939fb7d4cef757a22932deefe137ac7bdd944666
valiktorus <valiktorus@mail.ru>
2016-07-22 12:24:47 +0300

    init

--- src/com/javarush/test/level08/lesson08/task05/Solution.java
+++ src/com/javarush/test/level08/lesson08/task05/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.test.level08.lesson08.task05;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /* Удалить людей, имеющих одинаковые имена
 Создать словарь (Map<String, String>) занести в него десять записей по принципу «фамилия» - «имя».
@@ -30,6 +31,11 @@ public class Solution
     public static void removeTheFirstNameDuplicates(HashMap<String, String> map)
     {
         //напишите тут ваш код
+        Set<Map.Entry<String,String>> set = map.entrySet();
+        for (Map.Entry<String,String> entry: map.entrySet())
+        {
+
+        }
 
     }
 
