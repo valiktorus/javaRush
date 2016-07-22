ef042113c737212e6338b203784067a19f8f6742
valiktorus <valiktorus@mail.ru>
2016-07-22 12:25:17 +0300

    init

---
+++ src/com/javarush/test/level08/lesson11/bonus01/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level08.lesson11.bonus01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/* Номер месяца
+Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде: «May is 5 month».
+Используйте коллекции.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        //напишите тут ваш код
+    }
+
+}
