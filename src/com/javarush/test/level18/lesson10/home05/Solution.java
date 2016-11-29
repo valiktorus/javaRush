0dfebe756f9455d95df422fa6b2130d64119648a
valiktorus <valiktorus@mail.ru>
2016-11-29 18:03:36 +0300

    solution

---
+++ src/com/javarush/test/level18/lesson10/home05/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.test.level18.lesson10.home05;
+
+/* Округление чисел
+Считать с консоли 2 имени файла
+Первый файл содержит вещественные(дробные) числа, разделенные пробелом. Например, 3.1415
+Округлить числа до целых и записать через пробел во второй файл
+Закрыть потоки. Не использовать try-with-resources
+Принцип округления:
+3.49 - 3
+3.50 - 4
+3.51 - 4
+-3.49 - -3
+-3.50 - -3
+-3.51 - -4
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String firstName = reader.readLine();
+        String secondName = reader.readLine();
+
+
+
+    }
+}
