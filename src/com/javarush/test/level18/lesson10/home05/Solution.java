fff343d58913948316ccabe3562ffede497ae67d
valiktorus <valiktorus@mail.ru>
2016-11-30 18:24:41 +0300

    solution

--- src/com/javarush/test/level18/lesson10/home05/Solution.java
+++ src/com/javarush/test/level18/lesson10/home05/Solution.java
@@ -14,17 +14,27 @@ package com.javarush.test.level18.lesson10.home05;
 -3.51 - -4
 */
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String firstName = reader.readLine();
         String secondName = reader.readLine();
+        reader.close();
+        BufferedReader in = new BufferedReader(new FileReader(firstName));
+        String line  = in.readLine();
+        in.close();
+        String[] numbers = line.split(" ");
 
+        BufferedWriter writer = new BufferedWriter(new FileWriter(secondName));
 
-
+        for (String num: numbers) {
+            double number = Double.parseDouble(num);
+            Integer intNumber = (int)Math.round(number);
+            writer.write(intNumber.toString());
+            writer.write(" ");
+        }
+        writer.close();
     }
 }
---
+++ src/com/javarush/test/level18/lesson10/home06/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.test.level18.lesson10.home06;
+
+/* Встречаемость символов
+Программа запускается с одним параметром - именем файла, который содержит английский текст.
+Посчитать частоту встречания каждого символа.
+Отсортировать результат по возрастанию кода ASCII (почитать в инете). Пример: ','=44, 's'=115, 't'=116
+Вывести на консоль отсортированный результат:
+[символ1]  частота1
+[символ2]  частота2
+Закрыть потоки. Не использовать try-with-resources
+
+Пример вывода:
+, 19
+- 7
+f 361
+*/
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        Map<Integer,Integer> map = new TreeMap<>();
+        int simbol;
+        while ((simbol = reader.read()) !=-1){
+            if (map.containsKey(simbol)){
+
+            }
+        }
+
+
+    }
+}
