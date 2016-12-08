77e53d7f2098c435560091813d4bcfd7927c2759
valiktorus <valiktorus@mail.ru>
2016-12-08 22:42:20 +0300

    1

--- src/com/javarush/test/level19/lesson10/home05/Solution.java
+++ src/com/javarush/test/level19/lesson10/home05/Solution.java
@@ -7,7 +7,36 @@ package com.javarush.test.level19.lesson10.home05;
 Закрыть потоки. Не использовать try-with-resources
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 2) {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(args[1],true));
+            String line;
+            int count = 0;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    count++;
+                    String result = "";
+                    String[] lineSplit = line.split(" ");
+                    for (int i = 0; i < lineSplit.length; i++) {
+                        String word = lineSplit[i];
+                        if (word.matches(".*\\d.*")) {
+                            result += " " + word;
+                        }
+                    }
+                    if (count == 1) {
+                        writer.write(result.trim());
+                    } else {
+                        writer.write(result);
+                    }
+                }
+            }finally {
+                reader.close();
+                writer.close();
+            }
+        }
     }
 }
