013158d67966831fe9256b45e863e90e6c359a31
valiktorus <valiktorus@mail.ru>
2016-12-08 18:13:15 +0300

    solution

--- src/com/javarush/test/level19/lesson10/home03/Solution.java
+++ src/com/javarush/test/level19/lesson10/home03/Solution.java
@@ -1,7 +1,13 @@
 package com.javarush.test.level19.lesson10.home03;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* Хуан Хуанович
 В метод main первым параметром приходит имя файла.
@@ -22,7 +28,14 @@ import java.util.List;
 public class Solution {
     public static final List<Person> PEOPLE = new ArrayList<Person>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        String line;
+        while ((line = reader.readLine()) != null){
+            Pattern pattern = Pattern.compile("^([а-яА-Я\\s]+)\\s(\\d+)\\s(\\d+)\\s(\\d+)$");
+            Matcher matcher = pattern.matcher(line);
+
+        }
     }
 
 }
