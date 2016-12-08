4a3a28922fd31a31122fae1f4515de4e43eed08b
Torus <valiktorus@mail.ru>
2016-12-08 20:56:44 +0200

    solution

--- src/com/javarush/test/level19/lesson10/home03/Solution.java
+++ src/com/javarush/test/level19/lesson10/home03/Solution.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,15 +29,50 @@ import java.util.regex.Pattern;
 
 public class Solution {
     public static final List<Person> PEOPLE = new ArrayList<Person>();
+    public static void main(String[] args) throws Exception{
+        String fileName = args[0];
+        int day, month, year;
+        Calendar birthdayCalendar = new GregorianCalendar();
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+        while (fileReader.ready()){
+            String[] oneRow = fileReader.readLine().split(" ");
+            year = Integer.parseInt(oneRow[oneRow.length - 1]);
+            month = Integer.parseInt(oneRow[oneRow.length - 2]);
+            day = Integer.parseInt(oneRow[oneRow.length - 3]);
+            birthdayCalendar.set(year,month-1,day);
+            String name = new String();
+            for (int i = 0; i < oneRow.length - 3; i++)
+                if (i == oneRow.length-4){
+                    name +=oneRow[i];
+                }else {name += oneRow[i] + " ";}
+            PEOPLE.add(new Person(name, birthdayCalendar.getTime()));
+        }
+        fileReader.close();
+    }
+}
+/*public class Solution {
+    public static final List<Person> PEOPLE = new ArrayList<Person>();
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        BufferedReader reader = new BufferedReader(new FileReader("e:\\java\\projects\\javaRush\\src\\com\\javarush\\test\\level19\\lesson10\\home03\\1.txt"));
         String line;
         while ((line = reader.readLine()) != null){
-            Pattern pattern = Pattern.compile("^([а-яА-Я\\s]+)\\s(\\d+)\\s(\\d+)\\s(\\d+)$");
+            Pattern pattern = Pattern.compile("([а-яА-Я\\s]+)\\s(\\d+)\\s(\\d+)\\s(\\d+)");
             Matcher matcher = pattern.matcher(line);
-
+            if (!matcher.find()){
+                return;
+            }
+            String name = matcher.group(1);
+            int day = Integer.parseInt(matcher.group(2));
+            int mounth = Integer.parseInt(matcher.group(3));
+            int year = Integer.parseInt(matcher.group(4));
+            Calendar calendar = new GregorianCalendar(year,mounth -1,day);
+            PEOPLE.add(new Person(name,calendar.getTime()));
+        }
+        reader.close();
+        for (Person person: PEOPLE) {
+            System.out.println(person.getName());
+            System.out.println(person.getBirthday());
         }
     }
-
-}
+}*/
