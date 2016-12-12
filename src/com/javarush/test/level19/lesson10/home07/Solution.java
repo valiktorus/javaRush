98a17c1a57b637195ed3b643c91b2f1cf7691737
Torus <valiktorus@mail.ru>
2016-12-13 00:06:04 +0200

    solution

--- src/com/javarush/test/level19/lesson10/home07/Solution.java
+++ src/com/javarush/test/level19/lesson10/home07/Solution.java
@@ -9,23 +9,50 @@ package com.javarush.test.level19.lesson10.home07;
 Пример выходных данных:
 длинное,короткое,аббревиатура
 */
-
+import java.io.*;
+public class Solution {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader in1 = new BufferedReader(new FileReader(args[0]));
+        BufferedWriter out1 = new BufferedWriter(new FileWriter(args[1]));
+        while (in1.ready())
+        {
+            String test=in1.readLine();
+            String[] arr = test.split(" ");
+            for (int i = 0; i <arr.length ; i++){
+                if(arr[i].length()>6){
+                    if (i == arr.length-1){
+                        out1.write(arr[i]);
+                    }else {
+                        out1.write(arr[i] + ",");
+                    }
+                }
+            }
+        }
+        in1.close();
+        out1.close();
+    }
+}
+/*
 import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Solution {
+    private static final Pattern WORD_SEPARATED_BY_SPACE = Pattern.compile("\\S{7,}");
+    private static final String WORD_SEPARATOR = ",";
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(args[0]));
         BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
-        Pattern pattern = Pattern.compile("[^|\\s]([\\wа-яА-Я]{7,})[^|\\s]");
+        Pattern pattern = WORD_SEPARATED_BY_SPACE;
         Matcher matcher = pattern.matcher(reader.readLine());
         StringBuilder builder = new StringBuilder();
         while (matcher.find()){
-            builder.append(matcher.group() + ",");
+            builder.append(matcher.group() + WORD_SEPARATOR);
         }
         writer.write(builder.substring(0,builder.length()-1));
         reader.close();
         writer.close();
     }
 }
+*/
---
+++ src/com/javarush/test/level19/lesson10/home08/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.test.level19.lesson10.home08;
+
+/* Перевертыши
+1 Считать с консоли имя файла.
+2 Для каждой строки в файле:
+2.1 переставить все символы в обратном порядке
+2.2 вывести на экран
+3 Закрыть потоки. Не использовать try-with-resources
+
+Пример тела входного файла:
+я - программист.
+Амиго
+
+Пример результата:
+.тсиммаргорп - я
+огимА
+*/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = bufferedReader.readLine();
+        bufferedReader.close();
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        String line;
+        while ((line = reader.readLine()) != null){
+            StringBuilder builder = new StringBuilder();
+            builder.append(line);
+            builder.reverse();
+            System.out.println(builder);
+        }
+        reader.close();
+    }
+}
