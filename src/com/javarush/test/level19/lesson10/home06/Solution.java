db56a9486a771e07de2ecf476fb94261beab63c6
Torus <valiktorus@mail.ru>
2016-12-12 14:01:17 +0200

    solution

--- src/com/javarush/test/level19/lesson10/home06/Solution.java
+++ src/com/javarush/test/level19/lesson10/home06/Solution.java
@@ -3,10 +3,76 @@ package com.javarush.test.level19.lesson10.home06;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+/* Замена чисел
+1. В статическом блоке инициализировать словарь map парами [число-слово] от 0 до 12 включительно
+Например, 0 - "ноль", 1 - "один", 2 - "два"
+2. Считать с консоли имя файла
+3. Заменить все числа на слова используя словарь map
+4. Результат вывести на экран
+5. Закрыть потоки. Не использовать try-with-resources
+Пример данных:
+Это стоит 1 бакс, а вот это - 12 .
+Переменная имеет имя file1.
+110 - это число.
+Пример вывода:
+Это стоит один бакс, а вот это - двенадцать .
+Переменная имеет имя file1.
+110 - это число.
+*/
+public class Solution {
+    public static Map<Integer, String> map = new HashMap<Integer, String>();
+    static
+    {
+        map.put(12,"двенадцать");
+        map.put(11,"одинадцать");
+        map.put(10,"десять");
+        map.put(9,"девять");
+        map.put(8,"восемь");
+        map.put(7,"семь");
+        map.put(6,"шесть");
+        map.put(5,"пять");
+        map.put(4,"четыре");
+        map.put(3,"три");
+        map.put(2,"два");
+        map.put(1,"один");
+        map.put(0,"ноль");
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufreader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = bufreader.readLine();
+        bufreader.close();
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        while (reader.ready()){
+            String line=reader.readLine();
+            String[] data = line.split(" ");
+            for (int d =0;d<data.length;d++){
+                try
+                {
+                    for (Map.Entry<Integer,String> k:map.entrySet()){
+                        if(k.getKey()==Integer.parseInt(data[d]))
+                            data[d]=new String(k.getValue().getBytes(),"utf8");
+                    }
+                }catch (NumberFormatException e){
+                }
+                line="";
+                for (String v:data){
+                    line=line+v+" ";
+                }
+            }
+            line=line.substring(0,line.lastIndexOf(" "));
+            System.out.println(line);
+        }
+        reader.close();
+    }
+}
+/*
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/* Замена чисел
+*//* Замена чисел
 1. В статическом блоке инициализировать словарь map парами [число-слово] от 0 до 12 включительно
 Например, 0 - "ноль", 1 - "один", 2 - "два"
 2. Считать с консоли имя файла
@@ -23,7 +89,7 @@ import java.util.regex.Pattern;
 Это стоит один бакс, а вот это - двенадцать .
 Переменная имеет имя file1.
 110 - это число.
-*/
+*//*
 
 public class Solution {
     public static Map<Integer, String> map = new HashMap<Integer, String>();
@@ -50,19 +116,14 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new FileReader(fileName));
         String line;
         while ((line = reader.readLine()) != null){
-            String result = "";
-            Pattern patern = Pattern.compile("\\s(\\d{1,2})\\s");
-            Matcher matcher = patern.matcher(line);
-            while (matcher.find()){
-                int id = Integer.parseInt(matcher.group());
-                if (id<13) {
-                    result = matcher.replaceAll(map.get(id));
-                }
+
+            for (int i = 0; i <13 ; i++) {
+                line = line.replaceAll(("\\W(i)\\W")," " + map.get(i) + " ");
             }
-            System.out.println(result);
+            System.out.println(line);
 
         }
         reader.close();
 
     }
-}
+}*/
---
+++ src/com/javarush/test/level19/lesson10/home07/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.test.level19.lesson10.home07;
+
+/* Длинные слова
+В метод main первым параметром приходит имя файла1, вторым - файла2
+Файл1 содержит слова, разделенные пробелом.
+Записать через запятую в Файл2 слова, длина которых строго больше 6
+Закрыть потоки. Не использовать try-with-resources
+
+Пример выходных данных:
+длинное,короткое,аббревиатура
+*/
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
+        Pattern pattern = Pattern.compile("[^|\\s]([\\wа-яА-Я]{7,})[^|\\s]");
+        Matcher matcher = pattern.matcher(reader.readLine());
+        StringBuilder builder = new StringBuilder();
+        while (matcher.find()){
+            builder.append(matcher.group() + ",");
+        }
+        writer.write(builder.substring(0,builder.length()-1));
+        reader.close();
+        writer.close();
+    }
+}
