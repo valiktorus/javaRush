287bd2a8ca3a2889857a7164aeab559a6cb88436
Torus <valiktorus@mail.ru>
2016-12-13 22:50:28 +0200

    solution

--- src/com/javarush/test/level19/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level19/lesson10/bonus01/Solution.java
@@ -2,9 +2,7 @@ package com.javarush.test.level19.lesson10.bonus01;
 
 import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,32 +20,64 @@ file1:         file2:             результат:(lines)
 строка3        строка3            SAME строка3
 строка4                           REMOVED строка4
 строка5        строка5            SAME строка5
-строка0                           ADDED строка0
+               строка0            ADDED строка0
 строка1        строка1            SAME строка1
 строка2                           REMOVED строка2
 строка3        строка3            SAME строка3
-строка5                           ADDED строка5
+               строка5            ADDED строка5
 строка4        строка4            SAME строка4
 строка5                           REMOVED строка5
 */
 
 public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
-    public static List<String> firstList = new ArrayList<>();
-    public static List<String> secondList = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
-        String firstFileName = getFileName();
-        String secondFileName  = getFileName();
-        fillFirstList(firstFileName);
+        List<String> firstList;
+        List<String> secondList;
+//        String firstFileName = getFileName();
+//        String secondFileName  = getFileName();
+        String firstFileName;
+        String secondFileName;
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            firstFileName = reader.readLine();
+            secondFileName = reader.readLine();
+        }
+        firstList = fillList(firstFileName);
+        secondList = fillList(secondFileName);
+        fillResultList(firstList,secondList);
+
     }
 
-    private static String getFileName() throws IOException {
+/*    private static String getFileName() throws IOException {
         String fileName = null;
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             fileName = reader.readLine();
         }
         return fileName;
+    }*/
+    private static List<String> fillList(String fileName) throws IOException {
+        List<String> resultList = null;
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            resultList = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null){
+                resultList.add(line);
+            }
+        }
+        return resultList;
+    }
+    private static void fillResultList(List<String> firstList, List<String> secondLIst){
+        for (int i = 0; i < firstList.size() ; i++) {
+            String firstFileElement = firstList.get(i);
+            String secondFileElement = secondLIst.get(i);
+            if (firstFileElement.equals(secondFileElement)){
+                lines.add(new LineItem(Type.SAME,firstFileElement));
+            }else if (firstFileElement.isEmpty()){
+                lines.add(new LineItem(Type.ADDED,secondFileElement));
+                }else if (secondFileElement.isEmpty()){
+                lines.add(new LineItem(Type.REMOVED, firstFileElement));
+            }
+        }
     }
 
     public static enum Type {
